e03dbdbcf7b0b6423bb4f420144327bd1c9e119b
Mateja Petrovic <mateja176@gmail.com>
2019-02-25 23:47:52 +0100

    Adds basic marble test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,21 @@
-test("take off from here", () => {
-  expect(true).toBe(true);
-});
+import { marbles } from "rxjs-marbles/jest"
+import { map } from "rxjs/operators"
+
+describe("rxjs-marbles", () => {
+  it(
+    "should support marble tests",
+    marbles(m => {
+      const source = m.hot("--^-a-b-c-|")
+      const subs = "^-------!"
+      const expected = "--b-c-d-|"
+
+      const destination = source.pipe(
+        map(value => String.fromCharCode(value.charCodeAt(0) + 1)),
+      )
+
+      m.expect(destination).toBeObservable(expected)
+
+      m.expect(source).toHaveSubscriptions(subs)
+    }),
+  )
+})
